7b20ae78f920d644b4d532bb35e6016d408a6002
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2024-10-21 19:49:16 +0600

    feat: initiate application with proper folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'application-container',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ng-template';
-}
+export class AppComponent {}
